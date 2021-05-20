76575e40296d36521ac2589e20b0ae3d5b336454
sixg0000d <sixg0000d@gmail.com>
2021-05-20 22:40:22 +0800

    Add http mode

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"net/http"
+	"net/http/httputil"
 )
 
 func main() {
-	arg1 := flag.String("ip", "127.0.0.1", "`IP address` to listen")
-	arg2 := flag.Int("port", 9527, "`Port` to listen")
+	arg1 := flag.String("ip", "127.0.0.1", "which `IP` address to listen")
+	arg2 := flag.Int("port", 9527, "which `port` to listen")
+	arg3 := flag.String("mode", "tcp", "which `protocol` to use, possible value: tcp, http")
 	flag.Parse()
 	ip := net.ParseIP(*arg1)
 	if ip == nil {
@@ -19,13 +23,22 @@ func main() {
 	if port < 0 || port > 65535 {
 		log.Fatal("port number is out of range")
 	}
-
-	tcpAddr := net.TCPAddr{
+	addr := net.TCPAddr{
 		IP:   ip,
 		Port: port,
 	}
+	switch mode := *arg3; mode {
+	case "tcp":
+		tcpEcho(addr)
+	case "http":
+		httpEcho(addr)
+	default:
+		log.Fatal("unknown mode")
+	}
+}
 
-	l, err := net.ListenTCP("tcp", &tcpAddr)
+func tcpEcho(addr net.TCPAddr) {
+	l, err := net.ListenTCP("tcp", &addr)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,4 +53,17 @@ func main() {
 			c.Close()
 		}(conn)
 	}
+}
+
+func httpEcho(addr net.TCPAddr) {
+	echoHandler := func(w http.ResponseWriter, req *http.Request) {
+		dump, err := httputil.DumpRequest(req, true)
+		if err != nil {
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+		io.WriteString(w, string(dump))
+	}
+	http.HandleFunc("/", echoHandler)
+	log.Fatal(http.ListenAndServe(addr.String(), nil))
 }
